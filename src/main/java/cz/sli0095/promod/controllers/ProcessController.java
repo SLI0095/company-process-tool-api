e7b847d42e3ba55ba10224ea1c5d011634743081
Šimon Slíva <slisim98@gmail.com>
2023-04-10 12:16:38 +0200

    - cross origin can be set through properties

--- src/main/java/cz/sli0095/promod/controllers/ProcessController.java
+++ src/main/java/cz/sli0095/promod/controllers/ProcessController.java
@@ -20,7 +20,7 @@ import java.util.List;
 
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin
 public class ProcessController {
 
     @Autowired
--- src/main/java/cz/sli0095/promod/utils/WebConfig.java
+++ src/main/java/cz/sli0095/promod/utils/WebConfig.java
@@ -3,23 +3,33 @@ package cz.sli0095.promod.utils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
 
 @Configuration
 public class WebConfig {
 
-    @Value( "${webapp.url}" )
-    static String webAppUrl;
+    @Value("${promod.cors.allowed-origins:http://localhost:3000/}")
+    private String[] allowedOrigins;
+
+    @Value("${promod.cors.allowed-methods:GET,POST,PUT,DELETE,OPTIONS}")
+    private String[] allowedMethods;
+
+    @Value("${promod.cors.allowed-headers:Authorization,Content-Type}")
+    private String[] allowedHeaders;
 
     @Bean
-    public WebMvcConfigurer corsConfigurer()
-    {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
-            }
-        };
+    public CorsConfiguration corsConfiguration() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(Arrays.asList(allowedOrigins));
+        corsConfiguration.setAllowedMethods(Arrays.asList(allowedMethods));
+        corsConfiguration.setAllowedHeaders(Arrays.asList(allowedHeaders));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return corsConfiguration;
     }
 }
